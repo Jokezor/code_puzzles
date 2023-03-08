acc3733d29cbfcaf4ed2e3f6ed45f15708daa8ce
Jokezor <testifyingfor@gmail.com>
2023-03-08 07:26:03 +0100

    Need to learn this solution. Used factorial inverses and shit

--- arc058_b.cpp
+++ arc058_b.cpp
@@ -1,65 +1,36 @@
 #include <bits/stdc++.h>
 
-#define ll long long
-
 using namespace std;
 
+#define modN 1000000007
+#define ll long long
+
+long long H, W, A, B, K, ret, res, fact[300000], inv[300000], factinv[300000];
+
+long long ncr(int n, int r) {
+  return (n - r >= 0 && r >= 0)
+             ? 1LL * fact[n] * factinv[r] % modN * factinv[n - r] % modN
+             : 0;
+}
+
 void solution() {
-  int modN = 1000000007;
-  // What should we do?
-  ll H, W, A, B;
-
+  fact[0] = 1;
+  for (int i = 1; i < 300000; i++)
+    fact[i] = 1LL * fact[i - 1] * i % modN;
+  inv[1] = 1;
+  for (int i = 2; i < 300000; i++)
+    inv[i] = 1LL * inv[modN % i] * (modN - modN / i) % modN;
+  factinv[0] = 1;
+  for (int i = 1; i < 300000; i++)
+    factinv[i] = 1LL * factinv[i - 1] * inv[i] % modN;
   cin >> H >> W >> A >> B;
-
-  vector<vector<int>> dp(H);
-
-  for (int i = 0; i < H; i++) {
-    dp[i] = vector<int>(W);
-    dp[i][0] = 1;
+  long long sum = 0;
+  for (int i = 0; i < H - A; i++) {
+    sum += ncr(B + i - 1, B - 1) * ncr((W - B - 1) + (H - i - 1), W - B - 1);
+    sum %= modN;
   }
 
-  for (int j = 0; j < W; j++) {
-    dp[0][j] = 1;
-  }
-
-  for (int i = H - 1; i >= H - A; i--) {
-    for (int j = 0; j < B; j++) {
-      dp[i][j] = 0;
-    }
-  }
-
-  auto factorial = [&](int m) {
-    ll res = 1;
-    for (int i = 1; i <= m; i++) {
-      res = (res % modN) * (i % modN) % modN;
-    }
-    return res % modN;
-  };
-
-  cout << factorial(H + W - 2) % modN << endl;
-  cout << (factorial(H - 1) % modN) * factorial(W - 1) << endl;
-
-  ll all_combinations = (((factorial(H + W - 2) % modN)) /
-                         ((factorial(H - 1) * factorial(W - 1)) % modN));
-  ll combinations_removed =
-      (((factorial(A + B - 2) % modN)) /
-       (((factorial(A - 1) % modN) * (factorial(B - 1) % modN)) % modN));
-
-  cout << all_combinations << " " << combinations_removed << "\n";
-  cout << all_combinations - combinations_removed << "\n";
-
-  /*
-  for (int i = 1; i < H; i++) {
-    for (int j = 1; j < W; j++) {
-      if (i >= (H - A) && j < B) {
-        continue;
-      }
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-
-  cout << dp[H - 1][W - 1] % modN << "\n";
-  */
+  cout << sum << endl;
 }
 
 int main() {
@@ -72,3 +43,52 @@ int main() {
 
   return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+#define modN 1000000007;
+
+long long H, W, A, B, K, ret, res, fact[300000], inv[300000], factinv[300000];
+long long ncr(int n, int r) { return (n - r >= 0 && r >= 0) ? 1LL * fact[n] *
+factinv[r] % modN * factinv[n - r] % modN : 0; }
+
+void solution() {
+  // What should we do?
+  ll H, W, A, B;
+
+  cin >> H >> W >> A >> B;
+
+  auto factorial = [&](int m) {
+    ll res = 1;
+    for (int i = 1; i <= m; i++) {
+      res = (res % modN) * (i % modN) % modN;
+    }
+    return res % modN;
+  };
+
+  ll all_combinations = (((factorial(H + W - 2) % modN)) /
+                         ((factorial(H - 1) * factorial(W - 1)) % modN));
+  ll combinations_removed =
+      (((factorial(A + B - 2) % modN)) /
+       (((factorial(A - 1) % modN) * (factorial(B - 1) % modN)) % modN));
+
+  cout << all_combinations << " " << combinations_removed << "\n";
+  cout << all_combinations - combinations_removed << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+
+  while (t--)
+    solution();
+
+  return 0;
+}
+*/
