1963bc93bb149b5841ce571ef0e776f68fb0090d
Jokezor <testifyingfor@gmail.com>
2023-03-24 12:44:33 +0100

    Worked on understanding the solution more. Is still hard to know exactly how it works but I understand it more at least.

--- arc058_b.cpp
+++ arc058_b.cpp
@@ -9,20 +9,35 @@ long long H, W, A, B, K, ret, res, fact[300000], inv[300000], factinv[300000];
 
 long long ncr(int n, int r) {
   return (n - r >= 0 && r >= 0)
-             ? 1LL * fact[n] * factinv[r] % modN * factinv[n - r] % modN
+             ? fact[n] * factinv[r] % modN * factinv[n - r] % modN
              : 0;
 }
 
-void solution() {
+void calcFactorial() {
   fact[0] = 1;
-  for (int i = 1; i < 300000; i++)
-    fact[i] = 1LL * fact[i - 1] * i % modN;
+  for (int i = 1; i < 300000; i++) {
+    fact[i] = fact[i - 1] * i % modN;
+  }
+}
+
+void calcInverseFactorial() {
   inv[1] = 1;
-  for (int i = 2; i < 300000; i++)
-    inv[i] = 1LL * inv[modN % i] * (modN - modN / i) % modN;
+  for (int i = 2; i < 300000; i++) {
+    inv[i] = inv[modN % i] * (modN - modN / i) % modN;
+  }
   factinv[0] = 1;
-  for (int i = 1; i < 300000; i++)
-    factinv[i] = 1LL * factinv[i - 1] * inv[i] % modN;
+  for (int i = 1; i < 300000; i++) {
+    factinv[i] = factinv[i - 1] * inv[i] % modN;
+  }
+}
+
+void solution() {
+  // Calculate the factorials
+  calcFactorial();
+
+  // Calculate the inverse factorial
+  calcInverseFactorial();
+
   cin >> H >> W >> A >> B;
   long long sum = 0;
   for (int i = 0; i < H - A; i++) {
@@ -43,52 +58,3 @@ int main() {
 
   return 0;
 }
-
-/*
-#include <bits/stdc++.h>
-
-#define ll long long
-
-using namespace std;
-
-#define modN 1000000007;
-
-long long H, W, A, B, K, ret, res, fact[300000], inv[300000], factinv[300000];
-long long ncr(int n, int r) { return (n - r >= 0 && r >= 0) ? 1LL * fact[n] *
-factinv[r] % modN * factinv[n - r] % modN : 0; }
-
-void solution() {
-  // What should we do?
-  ll H, W, A, B;
-
-  cin >> H >> W >> A >> B;
-
-  auto factorial = [&](int m) {
-    ll res = 1;
-    for (int i = 1; i <= m; i++) {
-      res = (res % modN) * (i % modN) % modN;
-    }
-    return res % modN;
-  };
-
-  ll all_combinations = (((factorial(H + W - 2) % modN)) /
-                         ((factorial(H - 1) * factorial(W - 1)) % modN));
-  ll combinations_removed =
-      (((factorial(A + B - 2) % modN)) /
-       (((factorial(A - 1) % modN) * (factorial(B - 1) % modN)) % modN));
-
-  cout << all_combinations << " " << combinations_removed << "\n";
-  cout << all_combinations - combinations_removed << "\n";
-}
-
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  int t = 1;
-
-  while (t--)
-    solution();
-
-  return 0;
-}
-*/
