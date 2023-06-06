e98d83fea4559fcc0a5634ffcfe5fc5e1b5ab387
Jokezor <testifyingfor@gmail.com>
2023-06-06 08:00:02 +0200

    Solved problem with binary exponentiation

--- OJ_11029.cpp
+++ OJ_11029.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <sstream>
 
 #define ll long long
 
@@ -18,15 +17,27 @@ ll binpow(ll a, ll b, ll m) {
   return res;
 }
 
-string convert_to_string(ll m) {
-  ostringstream os;
-  os << setw(3) << setfill('0') << m;
-
-  return os.str();
+double leading_pow(double a, ll b) {
+  double res = 1.0;
+  while (b > 0) {
+    if (b & 1) {
+      res = res * a;
+    }
+    a = a * a;
+    while (res > 10) {
+      res /= 10;
+    }
+    while (a > 10) {
+      a /= 10;
+    }
+    b >>= 1;
+  }
+  return res;
 }
 
 void solution() {
-  ll n, k;
+  double n;
+  ll k;
 
   cin >> n >> k;
 
@@ -34,13 +45,18 @@ void solution() {
   // Only keep the highest parts in one and the lower parts in another.
   // Getting the first 3 digits is the same as the highest 3 digits.
   // This is the same as doing binpow % 1000.
+  // Keep only the highest 3 digits after each power!
+  // Keep only the lowest 3 digits for the trailing numbers!
+  // 9921 ^ 3 => 9921 ^ 2 = 98426241
+  // 984 * 9921
+  // We can actually always only use the leading 3 digits
+  double leading_digits = leading_pow(n, k);
+  double trailing_digits = binpow(n, k, 1000);
 
-  // Get highest 3
-  string highest_3 = convert_to_string(binpow(n, k, 1000000)).substr(0, 3);
+  cout << (int)(leading_digits * 100) << "..." << setw(3) << setfill('0')
+       << (int)(trailing_digits) << endl;
 
-  string lowest_3 = convert_to_string(binpow(n, k, 1000)).substr(0, 3);
-
-  cout << highest_3 << "..." << lowest_3 << endl;
+  // printf("%d...%d\n", (int)(leading_digits * 100), (int)trailing_digits);
 }
 
 int main() {
