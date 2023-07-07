0251e10c44d9d89c951ddfae785eaa31ba8f4eea
Jokezor <testifyingfor@gmail.com>
2023-07-07 07:48:04 +0200

    Solved 66, had to tweak eps to get much larger numbers.

--- project_euler/66.cpp
+++ project_euler/66.cpp
@@ -1,24 +1,17 @@
-#include <fstream>
-#include <gmpxx.h>
-#include <sstream>
-#include <string>
-#include <vector>
-
 #include <bits/stdc++.h>
+#include <gmpxx.h>
 #include <time.h>
-
-#define ll unsigned long long
 
 using namespace std;
 
-vector<mpz_class> continued_fractions(mpq_class a, mpq_class b) {
+vector<mpz_class> continued_fractions(mpf_class a, mpf_class b) {
   // https://en.wikipedia.org/wiki/Continued_fraction
-  double eps = 1e-6;
+  mpf_class eps = mpf_class("1e-400");
   vector<mpz_class> fractions;
 
   while (a > eps && b > eps) {
     fractions.push_back(mpz_class(a / b));
-    mpq_class temp = b;
+    mpf_class temp = b;
     b = a - b * mpz_class(a / b);
     a = temp;
   }
@@ -30,12 +23,15 @@ int main() {
 
   clock_t start = clock();
 
+  mpf_set_default_prec(10000);
+
   mpz_class max_h = 0;
-  int max_n = -1;
+  mpz_class max_n = -1;
 
   for (int n = 2; n <= 1000; n++) {
-    double a = sqrt(n);
-    double b = 1;
+    mpf_class a;
+    mpf_sqrt(a.get_mpf_t(), mpf_class(n).get_mpf_t());
+    mpq_class b = 1;
 
     // Start by getting the continued fractions
     vector<mpz_class> fractions = continued_fractions(a, b);
@@ -77,7 +73,7 @@ int main() {
     // Now take and calculate pell's approximation of h^2 - n*k^2 which should
     // be 1.
     for (int i = 2; i < h.size(); i++) {
-      if ((h[i] * h[i] - n * k[i] * k[i]) == 1) {
+      if ((h[i] * h[i] - mpz_class(n) * k[i] * k[i]) == 1) {
         cout << h[i] << " " << k[i] << "\n";
         if (h[i] > max_h) {
           max_h = h[i];
