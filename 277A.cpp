7c4511019f9bd9453bc907e35514d48de44d27fa
Jokezor <testifyingfor@gmail.com>
2023-09-04 18:39:36 +0200

    solved but could be simplified.

--- 277A.cpp
+++ 277A.cpp
@@ -42,14 +42,24 @@ public:
 };
 
 void solution() {
-  int n, m, ans;
+  int n, m;
+
+  int ans = 0;
 
   cin >> n >> m;
 
-  vector<vector<int>> languages(m);
   vector<vector<int>> employees(n);
 
-  // Union find the language sets.
+  // Use union find to get disjoint sets.
+  UnionFind uf;
+
+  uf.size = m;
+  uf.parent = vector<int>(m);
+  uf.rank = vector<int>(m);
+
+  for (int i = 0; i < m; i++) {
+    uf.make_set(i);
+  }
 
   for (int i = 0; i < n; i++) {
     int k;
@@ -63,44 +73,44 @@ void solution() {
     }
     for (int source_language : known_languages) {
       for (int target_language : known_languages) {
-        languages[source_language].push_back(target_language);
+        uf.union_sets(source_language, target_language);
       }
     }
     // Add the employees languages
     employees[i] = known_languages;
   }
 
-  // Use union find to get disjoint sets.
-  UnionFind uf;
-
-  uf.size = m;
-  uf.parent = vector<int>(m);
-  uf.rank = vector<int>(m);
-
-  for (int i = 0; i < m; i++) {
-    uf.make_set(i);
-  }
-
-  for (int i = 0; i < m; i++) {
-    for (int l : languages[i]) {
-      uf.union_sets(i, l);
-    }
-  }
-
   // Now lets check which are connected.
-  //
-  for (int e1 = 0; e1 < n; e1++) {
+  // Actually, we should not always start with employee 0.
+  // What if that employee only knows one language?
+  // Would be better to start with the employees that knows the most languages!
+
+  vector<pair<int, int>> cultural_employees(n);
+
+  for (int i = 0; i < n; i++) {
+    cultural_employees[i] = make_pair((int)employees[i].size(), i);
+  }
+
+  // Ensure the employees which knows the most languages goes first.
+  sort(cultural_employees.begin(), cultural_employees.end(),
+       [&](pair<int, int> p1, pair<int, int> p2) {
+         return p1.first > p2.first;
+       });
+
+  for (int i = 0; i < n; i++) {
+    int e1 = cultural_employees[i].second;
     if (employees[e1].size() == 0) {
       ans++;
       continue;
     }
-    bool connected = false;
-    int source_language;
-    int target_language;
 
-    for (int e2 = e1 + 1; e2 < n; e2++) {
+    for (int j = i + 1; j < n; j++) {
+      int e2 = cultural_employees[j].second;
+
       vector<int> e1_languages = employees[e1];
       vector<int> e2_languages = employees[e2];
+
+      bool connected = false;
 
       for (int l1 : e1_languages) {
         for (int l2 : e2_languages) {
@@ -109,17 +119,20 @@ void solution() {
           }
         }
       }
-
       if (!connected) {
-        if (e1_languages.size() != 0 && e2_languages.size() != 0) {
-          source_language = e1_languages[0];
-          target_language = e2_languages[0];
+        for (int l1 : e1_languages) {
+          for (int l2 : e2_languages) {
+            uf.union_sets(l1, l2);
+          }
         }
+        for (int l2 : e2_languages) {
+          employees[e1].push_back(l2);
+        }
+        for (int l1 : e1_languages) {
+          employees[e2].push_back(l1);
+        }
+        ans++;
       }
-    }
-    if (!connected) {
-      ans++;
-      uf.union_sets(source_language, target_language);
     }
   }
 
