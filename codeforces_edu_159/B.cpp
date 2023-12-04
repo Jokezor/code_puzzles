b27a3ca011403b69f05c4963645af2f590210b2e
Jokezor <testifyingfor@gmail.com>
2023-12-04 08:35:54 +0100

    Have to submit later. not sure if full solution.

--- codeforces_edu_159/B.cpp
+++ codeforces_edu_159/B.cpp
@@ -1,1 +1,58 @@
-// Not doing the rest today. Ironically need to rest today ;)
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+ll MAX = (ll)(1e9 + 7);
+
+int gcd(int x, int y) {
+  if (y) {
+    return gcd(y, x % y);
+  } else {
+    return x;
+  }
+}
+
+int lcm(int x, int y) { return (x / gcd(x, y)) * y; }
+
+void solution() {
+  ll n, p, l, t;
+  cin >> n >> p >> l >> t;
+
+  ll ans = 0;
+
+  // Take optimal multiples
+  ll optimal_days = ((n - 8) / 14) + (int)(n >= 8);
+
+  while (optimal_days > 0 && p > 0) {
+    ans++;
+    p -= (2 * t + l);
+    optimal_days--;
+  }
+
+  bool taken_task = false;
+  while (p > 0) {
+    if (n < 8 && !taken_task) {
+      p -= t;
+      taken_task = true;
+    }
+    p -= l;
+    ans++;
+  }
+
+  cout << n - ans << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+
+  cin >> t;
+
+  while (t--)
+    solution();
+
+  return 0;
+}
