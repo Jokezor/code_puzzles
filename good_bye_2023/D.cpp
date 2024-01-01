31758e69b637884a0623e503671e138f68eec22b
Jokezor <testifyingfor@gmail.com>
2024-01-01 11:12:15 +0100

    solved D with editorial and tried to do compile time computation

--- good_bye_2023/D.cpp
+++ good_bye_2023/D.cpp
@@ -21,8 +21,39 @@ int gcd(int x, int y) {
 
 int lcm(int x, int y) { return (x / gcd(x, y)) * y; }
 
+constexpr size_t arraySize = 99;
+
+constexpr int calculateValue(size_t i, size_t j) {
+  if (i == j) {
+    return i;
+  } else {
+    return 0;
+  }
+}
+
+constexpr std::array<std::array<int, arraySize>, arraySize> initializeArray() {
+  std::array<std::array<int, arraySize>, arraySize> arr{};
+  for (size_t i = 0; i < arraySize; ++i) {
+    for (size_t j = 0; j < arraySize; ++j) {
+      arr[i][j] = calculateValue(i, j);
+    }
+  }
+  return arr;
+}
+
+constexpr auto myArray = initializeArray();
+
+unordered_map<ll, vector<string>> powers;
+
 void solution() {
   // Solve it
+  // Need up to 99 length numbers.
+  ll n = 1;
+  cin >> n;
+
+  for (string num : powers[n]) {
+    cout << num << "\n";
+  }
 }
 
 int main() {
@@ -31,6 +62,28 @@ int main() {
   int t;
 
   cin >> t;
+
+  powers[1] = {"1"};
+  powers[3] = {"169", "961", "196"};
+
+  for (int i = 5; i <= 99; i += 2) {
+    for (auto p : powers[i - 2]) {
+      powers[i].push_back(p + "00");
+    }
+
+    string num_1(i, '0');
+    num_1[0] = '1';
+    num_1[i / 2] = '6';
+    num_1[i - 1] = '9';
+
+    string num_2(i, '0');
+    num_2[0] = '9';
+    num_2[i / 2] = '6';
+    num_2[i - 1] = '1';
+
+    powers[i].push_back(num_1);
+    powers[i].push_back(num_2);
+  }
 
   while (t--)
     solution();
