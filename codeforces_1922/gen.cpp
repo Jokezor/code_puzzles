aab7bb07e397c14c63edbe8d7b6bfb2bcf8473c0
Jokezor <testifyingfor@gmail.com>
2024-02-07 10:28:54 +0100

    solved D with my solution. Not padding the vector but checking the boundaries of the iterators.

--- codeforces_1922/gen.cpp
+++ codeforces_1922/gen.cpp
@@ -6,36 +6,24 @@ using namespace std;
 int main(int argc, char **argv) {
   srand(atoi(argv[1])); // srand sets the random seed
   int n = atoi(argv[2]);
-  int m = 1 + rand() % n;
+  int max_val = 1e9;
 
-  cout << "1\n";
   cout << n << "\n";
 
-  set<long long> a;
-  while (a.size() < n) {
-    a.insert(1 + rand() % n);
-  }
-  vector<long long> a_vec;
+  for (int i = 0; i < n; ++i) {
+    int m = 2 + rand() % n;
+    cout << m << "\n";
 
-  for (long long e : a) {
-    a_vec.push_back(e);
-  }
-  sort(a_vec.begin(), a_vec.end());
-
-  for (int i = 0; i < n - 1; ++i) {
-    cout << a_vec[i] << " ";
-  }
-  cout << a_vec[n - 1] << "\n";
-
-  cout << m << "\n";
-
-  for (int i = 0; i < m; ++i) {
-    int x = 1 + rand() % n;
-    int y = x;
-
-    while (y == x) {
-      y = 1 + rand() % n;
+    // a
+    for (int j = 0; j < m - 1; ++j) {
+      cout << 1 + rand() % max_val << " ";
     }
-    cout << x << " " << y << "\n";
+    cout << 1 + rand() % max_val << "\n";
+
+    // d
+    for (int j = 0; j < m - 1; ++j) {
+      cout << 1 + rand() % max_val << " ";
+    }
+    cout << 1 + rand() % max_val << "\n";
   }
 }
---
+++ codeforces_922/script.sh
@@ -0,0 +1,11 @@
+for i in `seq 2 50`; do
+    # prints the current test number
+    # I like to do this so I can see progress is being made
+    echo $i 
+
+    ./gen $i $i > input.txt
+    ./my_sol < input.txt > output.txt
+    ./sol < input.txt > answer.txt
+
+    diff output.txt answer.txt || break
+done
