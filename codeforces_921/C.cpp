3f4871011af477bf4c771ae335243ee45414de4c
Jokezor <testifyingfor@gmail.com>
2024-02-10 13:56:06 +0100

    solved A. B and C harder

---
+++ codeforces_921/C.cpp
@@ -0,0 +1,362 @@
+#include <algorithm>
+#include <bit>
+#include <bits/stdc++.h>
+#include <string>
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>,
+                         __gnu_pbds::rb_tree_tag,
+                         __gnu_pbds::tree_order_statistics_node_update>
+    ordered_set;
+
+ll MAX_VAL = (ll)(1e9 + 7);
+
+bool USE_TIMER = true;
+
+#if USE_TIMER
+#define MEASURE_FUNCTION()                                                     \
+  ScopedTimer timer { __func__ }
+#else
+#define MEASURE_FUNCTION()
+#endif
+
+class ScopedTimer {
+public:
+  using ClockType = std::chrono::steady_clock;
+  ScopedTimer(const char *func)
+      : function_name_{func}, start_{ClockType::now()} {}
+  ScopedTimer(const ScopedTimer &) = delete;
+  ScopedTimer(ScopedTimer &&) = delete;
+  auto operator=(const ScopedTimer &) -> ScopedTimer & = delete;
+  auto operator=(ScopedTimer &&) -> ScopedTimer & = delete;
+  ~ScopedTimer() {
+    using namespace std::chrono;
+    auto stop = ClockType::now();
+    auto duration = (stop - start_);
+    auto ms = duration_cast<milliseconds>(duration).count();
+    std::cout << ms << " ms " << function_name_ << '\n';
+  }
+
+private:
+  const char *function_name_{};
+  const ClockType::time_point start_{};
+};
+
+int gcd(int x, int y) {
+  if (y) {
+    return gcd(y, x % y);
+  } else {
+    return x;
+  }
+}
+
+int lcm(int x, int y) { return (x / gcd(x, y)) * y; }
+
+vector<ll> trial_division1(ll n) {
+  vector<ll> factorization;
+
+  for (ll d = 2; d * d <= n; d++) {
+    while (n % d == 0) {
+      factorization.push_back(d);
+      n /= d;
+    }
+  }
+  if (n > 1) {
+    factorization.push_back(n);
+  }
+  return factorization;
+}
+
+unordered_set<ll> divisors(ll n) {
+  unordered_set<ll> divisors;
+
+  for (int i = 1; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      divisors.insert(i);
+      divisors.insert(n / i);
+    }
+  }
+
+  return divisors;
+}
+
+ll num_divisors(ll num) {
+  ll total = 1;
+  for (int i = 2; (ll)i * i <= num; i++) {
+    if (num % i == 0) {
+      int e = 0;
+      do {
+        e++;
+        num /= i;
+      } while (num % i == 0);
+      total *= e + 1;
+    }
+  }
+  if (num > 1) {
+    total *= 2;
+  }
+  return total;
+}
+
+struct hash_pair {
+  template <class T1, class T2> size_t operator()(const pair<T1, T2> &p) const {
+    auto hash1 = hash<T1>{}(p.first);
+    auto hash2 = hash<T2>{}(p.second);
+
+    if (hash1 != hash2) {
+      return hash1 ^ hash2;
+    }
+
+    // If hash1 == hash2, their XOR is zero.
+    return hash1;
+  }
+};
+
+const int maxN = 200013;
+int n;
+
+vector<ll> p;
+
+void sieve_of_eratosthenes(ll n) {
+  vector<ll> isPrime(n + 1, true);
+
+  isPrime[0] = isPrime[1] = false;
+
+  for (ll i = 2; i <= n; i++) {
+    if (isPrime[i] && i * i <= n) {
+      for (ll j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  for (ll i = 0; i < n + 1; i++) {
+    if (isPrime[i]) {
+      p.push_back(i);
+    }
+  }
+}
+
+void print(auto &&r) {
+  std::ranges::for_each(r, [](auto &&i) { std::cout << i << ' '; });
+  cout << "\n";
+}
+
+struct custom_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM =
+        chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+
+struct hash_triplet {
+  template <class T1, class T2, class T3>
+  size_t operator()(const tuple<T1, T2, T3> &p) const {
+    auto hash1 = hash<T1>{}(get<0>(p));
+    auto hash2 = hash<T2>{}(get<1>(p));
+    auto hash3 = hash<T3>{}(get<2>(p));
+
+    size_t seed = 0;
+    seed ^= hash1 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= hash2 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= hash3 + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+
+    return seed;
+  }
+};
+
+template <class Os, class K> Os &operator<<(Os &os, const std::multiset<K> &v) {
+  os << '[' << v.size() << "] {";
+  bool o{};
+  for (const auto &e : v)
+    os << (o ? ", " : (o = 1, " ")) << e;
+  return os << " }\n";
+}
+
+// build
+// void build(int a[], ll v = 1, ll tl = 1, ll tr = n) {
+//   if (tl == tr) {
+//     // Go through all other elements and add
+//     // 1 to their score if they divide us.
+//     // Also count how many we divide to add our score.
+//     // Either best with a map?
+//     cout << a[tl] << "\n";
+//     for (int i = tl + 1; i <= n; ++i) {
+//       if (a[i] % a[tl] == 0) {
+//         cout << "hm\n";
+//         t[v]++;
+//       }
+//       if (a[tl] % a[i] == 0) {
+//         cout << "what\n";
+//         score[i]++;
+//       }
+//     }
+//     t[v] += score[tl];
+//     cout << t[v] << "\n";
+//     return;
+//   }
+//   ll tm = (tl + tr) >> 1;
+//
+//   build(a, v * 2, tl, tm);
+//   build(a, v * 2 + 1, tm + 1, tr);
+//
+//   // Unclear what to store at each non-leaf vertex
+//   // t[v] = max(t[2 * v], t[2 * v + 1]);
+// }
+
+// update
+// void update(ll l, ll r, ll pos, ll i, ll new_val, ll v = 1) {
+//   if (l == r) {
+//     t[v] = new_val;
+//     return;
+//   }
+//   ll mid = (l + r) >> 1;
+//   if (pos <= mid)
+//     update(l, mid, pos, i + 1, new_val, 2 * v);
+//   else
+//     update(mid + 1, r, pos, i + 1, new_val, 2 * v + 1);
+//
+//   if (i % 2 == 0) {
+//     if (N & 1) {
+//       t[v] = t[2 * v] | t[2 * v + 1];
+//     } else {
+//       t[v] = t[2 * v] ^ t[2 * v + 1];
+//     }
+//   } else {
+//     if (N & 1) {
+//       t[v] = t[2 * v] ^ t[2 * v + 1];
+//     } else {
+//       t[v] = t[2 * v] | t[2 * v + 1];
+//     }
+//   }
+// }
+//
+// // queries
+// unsigned int sum(ll l, ll r, ll v = 1, ll L = 1, ll R = n) {
+//   if (L > R)
+//     return 0;
+//   if (l == L && r == R) {
+//     return t[v];
+//   }
+//   ll mid = (l + r) >> 1;
+//
+//   unsigned int x, y;
+//
+//   x = sum(l, mid, v * 2, L, min(mid, R));
+//
+//   y = sum(mid + 1, r, v * 2 + 1, max(mid + 1, L), R);
+//
+//   if (N & 1) {
+//     return x | y;
+//   } else {
+//     return x ^ y;
+//   }
+// }
+
+void solution() {
+  // ScopedTimer timer{"solution"};
+  //
+  // Solve it
+  int k, m;
+  cin >> n >> k >> m;
+
+  string s;
+  cin >> s;
+
+  // Simply go through each letter up to k.
+  // Confirm that there are n of each letter after the first occurence.
+
+  // Thus we can simply push the indices of the letters
+  // Into a set
+  string failed_letter;
+  string s_ans = "";
+  unordered_map<ll, vector<ll>> letter_pos;
+
+  for (int i = 0; i < m; ++i) {
+    letter_pos[s[i]].push_back(i);
+  }
+
+  for (int i = 0; i < k; ++i) {
+    // find for each letter the count after the first index.
+    if (letter_pos[i + 'a'].size() < n) {
+      cout << "NO\n";
+      s_ans += i + 'a';
+      failed_letter = i + 'a';
+      for (int i = 0; i < n - 1; ++i) {
+        s_ans += failed_letter;
+      }
+      cout << s_ans << "\n";
+      return;
+    }
+    int i_start = letter_pos[i + 'a'].front();
+    int i_end = letter_pos[i + 'a'].back();
+
+    for (int j = 0; j < k; ++j) {
+      if (i == j) {
+        continue;
+      }
+      auto positions = letter_pos[j + 'a'];
+      // Find first greater than i_start
+      auto start_pos = lower_bound(positions.begin(), positions.end(), i_start);
+
+      if (distance(start_pos, positions.end()) < n - 1) {
+        cout << "NO\n";
+        s_ans += i + 'a';
+        failed_letter = j + 'a';
+        for (int i = 0; i < n - 1; ++i) {
+          s_ans += failed_letter;
+        }
+        cout << s_ans << "\n";
+        return;
+      }
+
+      // Find all before i_end
+      auto end_pos = lower_bound(positions.begin(), positions.end(), i_end);
+
+      if (distance(positions.begin(), end_pos) < n - 1) {
+        cout << "NO\n";
+        failed_letter = j + 'a';
+        for (int i = 0; i < n - 1; ++i) {
+          s_ans += failed_letter;
+        }
+        s_ans += i + 'a';
+        cout << s_ans << "\n";
+        return;
+      }
+    }
+  }
+
+  cout << "YES\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+    solution();
+
+  return 0;
+}
